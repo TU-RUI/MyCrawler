b4b63e024e7b415c5b178d8d18bdc13aae11430b
turui <991642404@qq.com>
2016-12-06 18:19:46 +0800

    无

--- src/main/java/com/crawler/CrawlerProcess.java
+++ src/main/java/com/crawler/CrawlerProcess.java
@@ -1,7 +1,9 @@
 package com.crawler;
 
 import com.entity.Request;
+import com.entity.Response;
 import com.handler.Handler;
+import com.http.HttpDownLoader;
 import com.queues.RedisQueue;
 
 public class CrawlerProcess implements Runnable{
@@ -14,9 +16,6 @@ public class CrawlerProcess implements Runnable{
         this.handler = handler;
         queue = crawler.getQueue();
     }
-    
-    
-    
     
     @Override
     public void run() {
@@ -31,6 +30,10 @@ public class CrawlerProcess implements Runnable{
                 if(request.getCurReqCount() >= request.getMaxReqCount()){
                     continue;
                 }
+                handler.before(request);
+                HttpDownLoader downLoader = new HttpDownLoader();
+                Response response = downLoader.precess(request);
+                handler.after(response);
             } catch (Exception e) {
                 // TODO: handle exception
             }
